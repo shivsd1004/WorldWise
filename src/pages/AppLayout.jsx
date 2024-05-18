8beb9c80736065dc0a2dd110334e9260d32ca98e
shivsd1004 <shivsd1234@gmail.com>
2024-05-18 19:06:00 +0530

    initial commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
+import User from "../components/User";
+export default function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+      <User />
+
+      <p>this</p>
+    </div>
+  );
+}
